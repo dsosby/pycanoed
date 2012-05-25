dec9a4871967d16659128da93fe2509591964dcc
David Sosby <dsosby@gmail.com>
2012-05-24 22:22:07 -0500

    Hide that mongo url again

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def hello():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    mongourl = os.environ.get('MONGOHQ_URL', "No URL")
+    #mongourl = os.environ.get('MONGOHQ_URL', "No URL")
     app.run(host='0.0.0.0',port=port)
