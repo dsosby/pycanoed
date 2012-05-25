9c553eccfd1de4b51d176b9d147dc29d6eda2131
David Sosby <dsosby@gmail.com>
2012-05-24 21:38:47 -0500

    Querying the mongohq url

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "pyCanoed"
+    return r"ipyCanoed<br/>{}".format(mongourl)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+    mongourl = os.environ.get('MONGOHQ_URL', "No URL")
     app.run(host='0.0.0.0',port=port)
