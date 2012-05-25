62bc40eb7b92379782495b33019d51f1b821e19a
David Sosby <dsosby@gmail.com>
2012-05-24 22:24:22 -0500

    Actually fix

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return r"ipyCanoed<br/>{}".format(mongourl)
+    return r"ipyCanoed"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    #mongourl = os.environ.get('MONGOHQ_URL', "No URL")
     app.run(host='0.0.0.0',port=port)
