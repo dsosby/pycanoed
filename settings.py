365dd311ff0d24bcc1e7709771e6fb29de6ddb4e
David Sosby <dsosby@gmail.com>
2012-05-28 17:27:36 -0500

    Fix bug when DATABASE_URL is not defined

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URL=os.environ.get('MONGOHQ_URL', os.environ['DATABASE_URL'])
+DATABASE_URL=os.environ.get('MONGOHQ_URL', os.environ.get('DATABASE_URL'))
 DEBUG=(os.environ.get('DEBUG').lower() == "true")
