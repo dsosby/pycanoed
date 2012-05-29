37a0193d5394ee13435890a2c62abd070a2fc245
David Sosby <dsosby@gmail.com>
2012-05-28 23:02:12 -0500

    Non-functioning post form

--- static/js/site.js
+++ static/js/site.js
@@ -1,5 +1,23 @@
 $(function()  {
-    $("#post_button").click(function()  {
+    $("#post-form").dialog({
+        autoOpen: false,
+        height:   400,
+        width:    600,
+        modal:    true,
+        buttons:  {
+            "Post": function()  {
+                alert("Posting");
+            },
+            Cancel: function()  {
+                $(this).dialog("close");
+            }
+        },
+        close: function()  {
+                $(this).dialog("close");
+        }
+    });
 
+    $("#post-button").click(function()  {
+        $("#post-form").dialog("open");
     });
 })();
