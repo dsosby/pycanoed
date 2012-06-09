2371f669c1c116e0e694736e539c08d958aa64d4
David Sosby <dsosby@gmail.com>
2012-06-09 16:13:07 -0500

    Auto focus post fields

--- static/js/site.js
+++ static/js/site.js
@@ -3,7 +3,10 @@ $(function()  {
         $.getJSON("/verify", {password: $("#post-password").val()}, function(result)  {
             if (result.valid)  {
                 $("#post-form").show();
+                $("#post-title").focus();
             }
         });
     });
+
+    $("#post-password").focus();
 });
